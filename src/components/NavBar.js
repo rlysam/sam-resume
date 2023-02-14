fee5d826fa2f45ba036f23c7bb3920a032b9e3e6
RlySam <bellosamuelb@gmail.com>
2023-02-14 14:02:45 +0800

    Before adding FontAwesome

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
+import NavIconLogo from "../assets/Nav-Icon.svg";
 
 const Navbar = () => {
 	return (
 		<nav className="navbar">
-			<h1 className="nav-icon">S</h1>
+			<h1 className="nav-icon">
+				<Link to="/">
+				<img src={ NavIconLogo } alt="" srcset="" className="nav-logo-icon" />	
+				</Link>
+			</h1>
 			<div className="links">
-				<Link to="/">Home</Link>
+				<Link to="/">
+					Home
+				</Link>
 				<Link to="/About">About</Link>
 				<Link to="/About">Skills</Link>
 				<Link to="/About">Portfolio</Link>
