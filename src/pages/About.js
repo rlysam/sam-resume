c3d22306696f0d9000c420fa6c870d87bd13af49
RlySam <bellosamuelb@gmail.com>
2023-02-20 01:08:27 +0800

    DONE About Page, Needs Square Boxes at the bottom

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,10 +3,12 @@
 // import { useEffect, useState } from "react";
 // import BlogList from "./BlogList";
 // import useFetch from "./useFetch";
+import { Link } from "react-router-dom";
+import { FacebookFill, LinkedinBoxFill, GithubFill, DiscordFill} from 'akar-icons';
 
 const About = () => {
 	return (
-		<div className="about">
+		<div className="inside-page about">
 			<div className="text-content">
 				<h1 className="title">About Me</h1>
 				<p className="body-text">
@@ -24,15 +26,14 @@ const About = () => {
 				</p>
 			</div>
 
-			<div className="socials">
-				<p>Hey</p>
-				<p>Hey</p>
-				<p>Hey</p>
-				<p>Hey</p>
-				<p>Hey</p>
-			</div>
+		<div className="socials">
+			<a href="https://www.google.com/" target="_blank"> <FacebookFill className="link-item" /> </a>
+			<a href="https://www.linkedin.com/in/bellosam/" target="_blank"> <LinkedinBoxFill className="link-item" /> </a>
+			<a href="https://github.com/rlysam" target="_blank"> <GithubFill className="link-item" /> </a>
+			<a href="https://www.google.com/" target="_blank"> <DiscordFill className="link-item" /> </a>
 		</div>
-	);
+	</div>
+);
 };
 
 export default About;
